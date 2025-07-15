c6ca5bd411d2c5504c57a15bbdf6fe2c9be53e13
yash bhatt <bhatty048@gmail.com>
2025-07-16 00:17:26 +0530

    feat : complete final version of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import Die from "./components/Die.jsx"
 import { nanoid } from 'nanoid';
 import {useState} from "react"
+import Confetti from 'react-confetti'
+
 export default function App(){
-    
+         function hold(id) {
+  setDice(prev =>
+    prev.map(item =>
+      item.id === id ? { ...item, isHeld: !item.isHeld } : item
+    )
+  );
+}
+ function reset(){
+    setDice(prev => prev.map(item => ({...item, isHeld : false , value :Math.ceil(Math.random() * 6)})))
+ }
+
         function allNewDice(){
             const result = [];
                 for (let i = 0; i < 10; i++) {
@@ -20,18 +32,27 @@ export default function App(){
             
           const [diceArr, setDice] = useState(generatedNumbers)
          function handleClick(){
-           setDice(prev => allNewDice())
+           setDice(prev => prev.map(die => die.isHeld ? die : {...die, value :Math.floor(Math.random() * 6) + 1} ))
          }
          const diceElements = diceArr.map((item)=>{
-                return <Die key = {item.id} value = {item.value} isHeld = {item.isHeld}/>
+                return <Die hold = {hold}
+                         id = {item.id} 
+                         key = {item.id} 
+                         value = {item.value} 
+                         isHeld = {item.isHeld}/>
             })
+ let gameWon = diceArr.every(die => die.isHeld) && 
+        diceArr.every(die => die.value === diceArr[0].value)
  
     return (
         <main>
+               {gameWon && <Confetti/> }
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <section className="dice">
                 {diceElements}
             </section>
-             <button onClick = {handleClick} className="roll">Roll</button>
+             <button onClick = {gameWon? reset : handleClick} className="roll">{gameWon ? "New Game" : "Roll"}</button>
         </main>
         
     )
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die(props){
     return(
 
-        <button className = "die" style={{backgroundColor : props.isHeld ? "lightgreen" : ""}}>{props.value}</button>
+        <button onClick ={()=>props.hold(props.id)} className = "die" style={{backgroundColor : props.isHeld ? "lightgreen" : ""}}>{props.value}</button>
     )
 }
